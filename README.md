c8261ce0c0061bc25dc25b535f8b13e0b927dcb8
Tom Shen <tom.shen@calix.com>
2022-09-22 11:16:19 +0800

    Support more

--- README.md
+++ README.md
@@ -4,14 +4,15 @@ This repo use the official [OpenWrt](https://openwrt.org/) to build a image for 
 [Ventoy](https://www.ventoy.net) is an open source tool to create bootable USB drive for ISO/WIM/IMG/VHD(x)/EFI files.
 You can use it to run [OpenWrt](https://openwrt.org/) on an USB drive easily.
 According to <https://github.com/ventoy/OpenWrtPlugin>, only `kmod-dax` and `kmod-dm` package is required.
-The official OpenWrt x86 image does not include these two packages, see this topics - <https://forum.openwrt.org/t/ventoy-hope-to-add-dm-kmod-to-the-img-by-default/94907>.
+The official OpenWrt x86 image does not include these two packages, see this topics <https://forum.openwrt.org/t/ventoy-hope-to-add-dm-kmod-to-the-img-by-default/94907>.
 You can use [Image Builder](https://openwrt.org/docs/guide-user/additional-software/imagebuilder).
 Another options is use [Image Builder docker image](https://hub.docker.com/r/openwrtorg/imagebuilder/tags?page=1&name=x86-64).
 This repo provide a script help you to create a image.
 
 Now you can use one of the following image
 
-- `docker.io/openwrtorg/imagebuilder:x86-64-21.02.2`
+- `docker.io/openwrtorg/imagebuilder:x86-64-22.03.0`
+- `docker.io/openwrtorg/imagebuilder:x86-64-21.02.3`
 - `docker.io/openwrtorg/imagebuilder:x86-64-19.07.9`
 - `docker.io/openwrtorg/imagebuilder:x86-64-18.06.7`, this version is not recommend
 
@@ -23,14 +24,21 @@ The following is another
 # export OPENWRT_MIRROR_PATH=https://mirrors.cloud.tencent.com/openwrt
 
 PACKAGES="${PACKAGES:+$PACKAGES }-wpad-mini -wpad-basic -dnsmasq"
-PACKAGES="${PACKAGES:+$PACKAGES }atop bash bind-dig coreutils-base64 curl diffutils dnsmasq-full dropbearconvert fdisk file \
+PACKAGES="${PACKAGES:+$PACKAGES }dnsmasq-full wpad \
+atop bash bind-dig coreutils-base64 curl diffutils dropbearconvert fdisk file \
 ip-full ipset \
 lscpu \
 luci luci-theme-bootstrap \
 nano pciutils procps-ng-pkill tcpdump tmux \
-uci wget wpad"
+uci wget"
 export PACKAGES
 
-version=21.02.2
-bash build.sh -b bin-$version -v $version
+version=21.02.3
+bash build.sh -v $version
+```
+
+```bash
+bash build.sh -p "luci-app-vlmcsd" -t /work/openwrt/package/22.03/x86-64
+
+env PACKAGES="luci-app-vlmcsd" bash build.sh -t /work/openwrt/package/22.03/x86-64
 ```
--- build.sh
+++ build.sh
@@ -2,36 +2,86 @@
 
 set -e
 
+THIS_DIR=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "${THIS_DIR}")
+
+trap _exec_exit_hook EXIT
+function _exec_exit_hook() {
+    local _idx
+    for ((_idx = ${#_EXIT_HOOKS[@]} - 1; _idx >= 0; _idx--)); do
+        eval "${_EXIT_HOOKS[_idx]}" || true
+    done
+}
+
+function _add_exit_hook() {
+    while (($#)); do
+        _EXIT_HOOKS+=("$1")
+        shift
+    done
+}
+
+PACKAGES=${PACKAGES:-""}
+
+function _add_package() {
+    local _before=0
+    if [[ ${1} == "-b" ]]; then
+        _before=1
+        shift
+    fi
+    while (($#)); do
+        if [[ ${PACKAGES} != *"${1}"* ]]; then
+            if [[ ${_before} -gt 0 ]]; then
+                PACKAGES="${1}${PACKAGES:+ ${PACKAGES}}"
+            else
+                PACKAGES="${PACKAGES:+${PACKAGES} }${1}"
+            fi
+        fi
+        shift
+    done
+}
+
+_add_package -b wpad dnsmasq-full
+_add_package -b "-wpad-mini" "-wpad-basic" "-dnsmasq"
+_add_package luci luci-theme-bootstrap
+# kmod-dax kmod-dm is required for ventoy
+_add_package kmod-dax kmod-dm
+
+function _check_param() {
+    while (($#)); do
+        if [[ -z ${!1} ]]; then
+            echo "\${$1} is required"
+            return 1
+        fi
+        shift 1
+    done
+}
+
 function print_usage() {
     #shellcheck disable=SC2016
     cat <<EOF
 Usage: $(basename "${BASH_SOURCE[0]}") [OPTIONS]
 OPTIONS
     -h, show help.
-    -a PACKAGES, additional packages. ${PACKAGES:+The default is "${PACKAGES}"}
-    -b BINDING, the bin directory binding for image output. ${BINDING:+The default is '"${BINDING}"'}
+    -b BINDIR, the output directory for image. ${BINDIR:+The default is '"${BINDIR}"'}
     -c, clean build. ${CLEAN:+The default is "${CLEAN}"}
+    -d, dry run. ${DRYRUN:+The default is '"${DRYRUN}"'}
     -k KUSTOMIZE, the directory for customized files, refer to https://openwrt.org/docs/guide-developer/uci-defaults. ${KUSTOMIZE:+The default is "${KUSTOMIZE}"}
     -n, the customize name. ${NAME:+The default is '"${NAME}"'}
-    -v VERSION, the openwrt version. ${VERSION:+The default is '"${VERSION}"'}
+    -p PACKAGES, additional packages. ${PACKAGES:+The default is '"${PACKAGES}"'}
+    -t THIRDPARTY, Thirdparty package directory. ${THIRDPARTY:+The default is '"${THIRDPARTY}"'}
+    -v VERSION, the OpenWRT version. ${VERSION:+The default is '"${VERSION}"'}
 EOF
 }
 
-BINDING=${BINDING:-"$PWD/bin"}
+BINDIR=${BINDIR:-""}
 CLEAN=0
-DOCKER_IMAGE=docker.io/openwrtorg/imagebuilder:x86-64
-KUSTOMIZE=${KUSTOMIZE:-""}
+DRYRUN=0
+KUSTOMIZE=${KUSTOMIZE:-"${THIS_DIR}/config"}
 NAME=${NAME:-default}
-PACKAGES=${PACKAGES:+${PACKAGES} }"kmod-dax kmod-dm" # kmod-dax kmod-dm is required for ventoy
-VERSION=${VERSION:-"21.02.2"}
+THIRDPARTY=${THIRDPARTY:-""}
+VERSION=${VERSION:-"22.03.0"}
 
-_cmd=""
-if [[ $(timedatectl show | grep Timezone | cut -d= -f2) == Asia/Shanghai ]]; then
-    OPENWRT_MIRROR_PATH=${OPENWRT_MIRROR_PATH:-http://mirrors.ustc.edu.cn/openwrt}
-    _cmd=${_cmd:+${_cmd}; }"sed -i -e \"s|http://downloads.openwrt.org|${OPENWRT_MIRROR_PATH}|g\" -e \"s|https://downloads.openwrt.org|${OPENWRT_MIRROR_PATH}|g\" repositories.conf"
-fi
-
-while getopts "ha:b:ck:n:v:" OPTION; do
+while getopts "hb:cdk:n:p:t:v:" OPTION; do
     case $OPTION in
     h)
         print_usage
@@ -40,17 +90,23 @@ while getopts "ha:b:ck:n:v:" OPTION; do
     c)
         CLEAN=1
         ;;
-    a)
-        PACKAGES="${PACKAGES:+$PACKAGES }${OPTARG}"
+    d)
+        DRYRUN=1
         ;;
     b)
-        BINDING=${OPTARG}
+        BINDIR=$(readlink -f "${OPTARG}")
         ;;
     k)
-        KUSTOMIZE=${OPTARG}
+        KUSTOMIZE=$(readlink -f "${OPTARG}")
         ;;
     n)
         NAME=${OPTARG}
+        ;;
+    p)
+        PACKAGES="${PACKAGES:+$PACKAGES }${OPTARG}"
+        ;;
+    t)
+        THIRDPARTY=$(readlink -f "${OPTARG}")
         ;;
     v)
         VERSION=${OPTARG}
@@ -62,35 +118,73 @@ while getopts "ha:b:ck:n:v:" OPTION; do
     esac
 done
 
-if [[ ${CLEAN} -gt 0 && -d "${BINDING}" ]]; then
-    rm -fr "${BINDING}"
-fi
-if [[ ! -d ${BINDING} ]]; then
-    mkdir -p "${BINDING}"
+_check_param VERSION
+if [[ -z ${BINDIR} ]]; then
+    BINDIR=${THIS_DIR}/${VERSION}-bin
 fi
 
-docker_cmd="docker run --rm -t"
-#shellcheck disable=SC2086
-docker_cmd=${docker_cmd:+${docker_cmd} }"-u build:$(id -gn) --group-add $(id -gn) -v $(readlink -f ${BINDING}):/home/build/openwrt/bin"
+if [[ ${CLEAN} -gt 0 && -d "${BINDIR}" ]]; then
+    rm -fr "${BINDIR}"
+fi
+if [[ ! -d ${BINDIR} ]]; then
+    mkdir -p "${BINDIR}"
+fi
+
+docker_image_name=docker.io/openwrtorg/imagebuilder:x86-64-${VERSION}
+docker image pull "${docker_image_name}"
+
+docker_opts=(--rm -it -u "$(id -u):$(id -g)" -v "${BINDIR}":/home/build/openwrt/bin)
+makecmd="make image"
+if [[ $(timedatectl show | grep Timezone | cut -d= -f2) == Asia/Shanghai ]]; then
+    OPENWRT_MIRROR_PATH=${OPENWRT_MIRROR_PATH:-http://mirrors.ustc.edu.cn/openwrt}
+    cmd=${cmd:+${cmd}; }"sed -i -e 's|http://downloads.openwrt.org|${OPENWRT_MIRROR_PATH}|g' -e 's|https://downloads.openwrt.org|${OPENWRT_MIRROR_PATH}|g' repositories.conf"
+fi
 for item in http_proxy https_proxy no_proxy; do
     if [[ -n ${!item} ]]; then
-        docker_cmd=${docker_cmd:+${docker_cmd} }"--env ${item}=${!item} --env ${item^^}=${!item}"
+        docker_opts+=(--env "${item}=${!item}")
     fi
 done
+if [[ -n ${THIRDPARTY} ]]; then
+    docker_opts+=(-v "${THIRDPARTY}:/home/build/openwrt/thirdparty")
+    cmd="${cmd:+${cmd}; }sed -i -e '\|^## Place your custom repositories here.*|a src custom file:///home/build/openwrt/thirdparty' -e 's/^option check_signature$/# &/' repositories.conf"
+fi
 
-_cmd=${_cmd:+${_cmd}; }"make image EXTRA_IMAGE_NAME=${NAME}"
 if [[ -n ${KUSTOMIZE} ]]; then
+    docker_opts+=(-v "${KUSTOMIZE}:/home/build/openwrt/custom")
     _cmd="${_cmd} FILES=/home/build/customize"
     #shellcheck disable=SC2086
     docker_cmd=${docker_cmd:+${docker_cmd} }"-v $(readlink -f ${KUSTOMIZE}):/home/build/customize"
+    if [[ -n ${OPENWRT_MIRROR_PATH} ]]; then
+        mkdir -p "${KUSTOMIZE}/etc/uci-defaults"
+        cat <<EOF > "${KUSTOMIZE}/etc/uci-defaults/10_opkg"
+#!/bin/sh
+
+sed -i -e 's|https://downloads.openwrt.org|${OPENWRT_MIRROR_PATH}|g' -e 's|http://downloads.openwrt.org|${OPENWRT_MIRROR_PATH}|g' /etc/opkg/distfeeds.conf
+# sed -i -e 's|${OPENWRT_MIRROR_PATH}|http://downloads.openwrt.org|g' /etc/opkg/distfeeds.conf
+
+exit 0
+EOF
+    fi
+
+    makecmd="${makecmd} FILES=/home/build/openwrt/custom"
+    _add_exit_hook "rm -f ${KUSTOMIZE}/etc/uci-defaults/10_opkg"
+fi
+
+if [[ -n ${NAME} ]]; then
+    makecmd="${makecmd} EXTRA_IMAGE_NAME=${NAME}"
 fi
 if [[ -n ${PACKAGES} ]]; then
-    _cmd="${_cmd} PACKAGES=\"${PACKAGES}\""
+    makecmd="${makecmd} PACKAGES=\"${PACKAGES}\""
 fi
 
-eval "${docker_cmd} ${DOCKER_IMAGE}-${VERSION} bash -c '${_cmd}'"
+if [[ ${DRYRUN} -eq 0 ]]; then
+    docker run "${docker_opts[@]}" "${docker_image_name}" bash -c "${cmd}; ${makecmd}"
+else
+    echo "${makecmd}"
+    docker run "${docker_opts[@]}" "${docker_image_name}" bash -c "${cmd}; bash"
+fi
 
-if [[ $(command -v qemu-img) ]]; then
+if [[ $(command -v qemu-img) && ${DRYRUN} -eq 0 ]]; then
     while IFS= read -r _gz_image; do
         _prefix=$(dirname "${_gz_image}")
         _img=${_prefix}/$(basename -s .gz "${_gz_image}")
@@ -104,5 +198,5 @@ if [[ $(command -v qemu-img) ]]; then
         qemu-img convert -c -O qcow2 "${_img}" "${_qcow}"
         qemu-img convert -O qcow2 "${_qcow}" "${_img}" # Ventoy use img
         unset -v _prefix _img _qcow
-    done < <(find "${BINDING}/targets/x86/64" -iname "*-combined*.img.gz" | sort)
+    done < <(find "${BINDIR}/targets/x86/64" -iname "*-combined*.img.gz" | sort)
 fi
